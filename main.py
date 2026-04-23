0aaf3a20345e7615216100a236c37863f75762fe
Кирилл <kiril.j2018@yandex.ru>
2026-04-23 23:28:02 +0300

    добавление функции и логирования

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print ("Hello world")
+import logging
+logging.basicConfig(level=logging.INFO)
+
+def greet(name: str) -> str:
+    logging.info("Вызов функции greet")
+    return f"Привет, {name}!"
